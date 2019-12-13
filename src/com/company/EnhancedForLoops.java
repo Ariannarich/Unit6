c9430fcda9dc9023544bbcdc5f559ff2af6b05f1
richaar <arianna.richardson@ssfs.org>
2019-12-13 10:01:34 -0500

    started enhanced

--- src/com/company/EnhancedForLoops.java
+++ src/com/company/EnhancedForLoops.java
@@ -1,4 +1,61 @@
 package com.company;
 
 public class EnhancedForLoops {
+
+    public static String printStandardFor(String[] arr){
+        String blank ="";
+        for(int i = 0;i< arr.length; i++)
+            blank += arr[i] + " ";
+        return blank;
+    }
+
+    public static String printStandardFor(int[] arr){
+        String blank ="";
+        for(int i = 0;i< arr.length; i++)
+            blank += arr[i] + " ";
+        return blank;}
+
+    public static String printStandardFor(double[] arr) {
+        String blank ="";
+        for(int i = 0;i< arr.length; i++)
+            blank += arr[i] + " ";
+        return blank;
+    }
+
+    public static String printEnhancedFor(String[] arr){
+        String blank ="";
+        return blank;
+    }
+
+    public static String printEnhancedFor(int[] arr){
+        String blank ="";
+        return blank;
+    }
+
+    public static String printEnhancedFor(double[] arr){
+        String blank ="";
+        return blank;
+    }
+
+    public static String printStandardWhile(String[] arr) {
+        String blank ="";
+        return blank;
+    }
+
+    public static String printStandardWhile(int[] arr) {
+        String blank ="";
+        return blank;
+    }
+
+    public static String printStandardWhile(double[] arr) {
+        String blank ="";
+        return blank;
+    }
+
+    public static void main(String[] args) {
+        int [] integer= {1,2,3,4,5};
+        double [] d ={1.0,2.0,3.0,4.0,5.0,};
+        String [] str= {"one", "two", "three", "four", "five"};
+    }
+
 }
