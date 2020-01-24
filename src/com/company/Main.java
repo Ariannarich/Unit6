701f1b94214537cdcacb13890cdd014ffdda8efe
arian <arianna.richardson@ssfs.org>
2020-01-23 20:25:50 -0500

    Printing corrected

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int num = 913742685;
+	int x, y = 0;
+	while (num > 0)
+    {
+        x = num % 10;
+        num/=10;
+        if (x>= 5)
+            y++;
+    }
+	System.out.println(y);
     }
 }
